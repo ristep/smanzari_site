aa0d2517d4ef9c2b85070f91992e37c37e1486e8
ristep <ristepan@gmail.com>
2026-02-05 17:03:49 +0100

    Ajde A02

--- gdrive_backup.sh
+++ gdrive_backup.sh
@@ -9,6 +9,3 @@ rclone copy -P --max-depth 1 --drive-chunk-size 64M smanzy_data/uploads/ gdrive:
 # Remove files older than 30 days
 find /home/ristepan/smanzari_site/backups -type f -mtime +30 -delete
 find /home/ristepan/smanzari_site/smanzy_data/uploads -type f -mtime +30 -delete
-
-
-# testing mestin u pm
